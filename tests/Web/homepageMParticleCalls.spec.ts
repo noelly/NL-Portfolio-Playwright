df6ac894889b6d30d922f40d1297c58fca74e701
Noel Ly <21228092+noelly@users.noreply.github.com>
2025-01-15 15:20:36 -0800

    MT-HP-calls

--- tests/Web/homepageMParticleCalls.spec.ts
+++ tests/Web/homepageMParticleCalls.spec.ts
@@ -1,12 +1,21 @@
 import { test, expect } from '@playwright/test';
+import * as fs from 'fs';
 
 test('Verify MParticle call is triggered for Motortrend', async ({ page }) => {
+  const requestUrls: string[] = [];
+
   await page.route(/mparticle.com\/*\/.*/, async (route, request) => {
-    console.log('Request URL:', request.url());
-    await route.continue();
+    const url = request.url();
+    requestUrls.push(url);
+    console.log('Request URL:', url);
   });
 
   await page.goto('https://www.motortrend.com/');
+
+
+  // Export the list of request URLs to a data file
+  const data = JSON.stringify(requestUrls, null, 2);
+  fs.writeFileSync('data/requestMparticleUrls.json', data);
 
 
   page.on('response', async (response) => {
